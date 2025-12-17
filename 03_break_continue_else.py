3bcc033b53e9df010a578e25ee214cd136501686
ibrahim-kiri <ibrahimkiri@gmail.com>
2025-12-17 09:51:48 +0300

    Added break,continue,else in for loops

---
+++ 03_break_continue_else.py
@@ -0,0 +1,24 @@
+for i in range(1, 6):
+    print(i)
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+for i in range(3):
+    print(i)
+else:
+    print("Loop completed")
+
+for i in range(5):
+    if i == 2:
+        break
+    print(i)
+else:
+    print("Loop completed")
